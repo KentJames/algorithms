6054028df377f12a9a29a18052cc836fd80895bc
James Kent <jameschristopherkent@gmail.com>
2017-03-17 15:44:35 +0000

    Multithreaded matrix mean reduction now working! Thanks to Bojan Nikolic for correcting my incorrect knowledge on pointers.

--- arrays/reduce_mean/reduce_mean_threaded.c
+++ arrays/reduce_mean/reduce_mean_threaded.c
@@ -4,10 +4,7 @@
 #include <pthread.h>
 #include <err.h>
 
-#define THREAD_NUM 4
 //Data structure passed to each threaded routine to bound where it works on the array.
-//
-void printmatrix(double*,int);
 
 struct arrayindex {
 
@@ -28,58 +25,51 @@ struct thread_data {
     struct arrayindex bounds;
 };
 
-//reduces matrix size by 2 in each dimension
-void *matrixreduce(void *threadArg){
+//reduces matrix size by 2 in each dimension.
+void *matrixreduce_worker(void *threadArg){
 
     struct thread_data *inst_thread_data = (struct thread_data *) threadArg;
  
-    if(inst_thread_data->dimensions%inst_thread_data->reduction) errx(1,"dimension/reduction mismatch!");
-
-
-    
-    printf("\n Reduction: %d \n Dimensions: %d \n Imin: %d \n Imax: %d \n Jmin: %d \n Jmax: %d \n",inst_thread_data->reduction,inst_thread_data->dimensions,
-            inst_thread_data->bounds.imin,inst_thread_data->bounds.imax,inst_thread_data->bounds.jmin,inst_thread_data->bounds.jmax);
-    //printmatrix(*(inst_thread_data->inputmatrix),inst_thread_data->dimensions);
+    if(inst_thread_data->dimensions%inst_thread_data->reduction) errx(1,"dimension/reduction mismatch!"); 
+    //printf("\n Reduction: %d \n Dimensions: %d \n Imin: %d \n Imax: %d \n Jmin: %d \n Jmax: %d \n",inst_thread_data->reduction,inst_thread_data->dimensions,
+       //     inst_thread_data->bounds.imin,inst_thread_data->bounds.imax,inst_thread_data->bounds.jmin,inst_thread_data->bounds.jmax);
     int outputdimensions = inst_thread_data->dimensions/inst_thread_data->reduction;
-    inst_thread_data->bounds.imax = outputdimensions;
 
     int i,j;
-    //pthread_mutex_lock(&inst_thread_data->thread_mutex);
+
     for(j=inst_thread_data->bounds.jmin;j<=inst_thread_data->bounds.jmax;j+=1){ 
         for(i=inst_thread_data->bounds.imin;i<=inst_thread_data->bounds.imax;i+=1){
             int l,m;
-            *(inst_thread_data->outputmatrix)[i*outputdimensions+j]=0.0;
+            *(*(inst_thread_data->outputmatrix)+i*outputdimensions+j) = 0.0;
             for(m=0;m<inst_thread_data->reduction;m++){
                 for(l=0;l<inst_thread_data->reduction;l++){
-                    printf("Attempt %d %d\n",l,m);
-                    *(inst_thread_data->outputmatrix)[i*outputdimensions+j] += *(inst_thread_data->inputmatrix)[(i*inst_thread_data->reduction+l)*inst_thread_data->dimensions+(j*inst_thread_data->reduction+m)];
+                    *(*(inst_thread_data->outputmatrix)+i*outputdimensions+j) += *(*(inst_thread_data->inputmatrix)+(((i*inst_thread_data->reduction)+l)*(inst_thread_data->dimensions))+((j*inst_thread_data->reduction)+m));
                 }
             }
-            *(inst_thread_data->outputmatrix)[i*outputdimensions+j] /= pow(2,inst_thread_data->reduction);
-
-            //printf("Value: %f \n",*(inst_thread_data->outputmatrix)[i*outputdimensions+j]);
+            *(*(inst_thread_data->outputmatrix)+i*outputdimensions+j) /= pow(2,inst_thread_data->reduction);
         }
    }
-    //pthread_mutex_unlock(&inst_thread_data->thread_mutex);
 }
 
 
 //sliding window
-void slidingmatrixreduce(double *inputmatrix, double *outputmatrix, int dimensions,int reduction){
+void *slidingmatrixreduce_worker(void *threadArg){
 
-    if(dimensions%reduction) errx(1,"dimension/reduction mismatch!");
-    int outputdimensions = dimensions - 1;
+    struct thread_data *inst_thread_data = (struct thread_data *) threadArg;
+
+    if(inst_thread_data->dimensions%inst_thread_data->reduction) errx(1,"dimension/reduction mismatch!");
+    int outputdimensions = inst_thread_data->dimensions - 1;
     int i,j;
-    for(j=0;j<outputdimensions;j++){
-        for(i=0;i<outputdimensions;i++){
+    for(j=inst_thread_data->bounds.jmin;j<inst_thread_data->bounds.jmax;j++){
+        for(i=inst_thread_data->bounds.imin;i<inst_thread_data->bounds.imax;i++){
             int l,m;
-            outputmatrix[i*outputdimensions+j] = 0.0;
-            for(m=0;m<reduction;m++){
-                for(l=0;l<reduction;l++){
-                    outputmatrix[i*outputdimensions+j] += inputmatrix[(l+i)*dimensions+(j+m)];
+            *(*(inst_thread_data->outputmatrix)+i*outputdimensions+j) = 0.0;
+            for(m=0;m<inst_thread_data->reduction;m++){
+                for(l=0;l<inst_thread_data->reduction;l++){
+                    *(*(inst_thread_data->outputmatrix)+i*outputdimensions+j) += *(*(inst_thread_data->inputmatrix)+(l+i)*inst_thread_data->dimensions+(j+m));
                 }
             }
-            outputmatrix[i*outputdimensions+j] /= pow(2,reduction);
+            *(*(inst_thread_data->outputmatrix)+i*outputdimensions+j) /= pow(2,inst_thread_data->reduction);
         }
     }
 
@@ -124,19 +114,21 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
-    struct thread_data thread_desc[threads];
-    
+    struct thread_data *thread_desc = malloc(threads*sizeof(struct thread_data));
+    struct thread_data *thread_desc_slide = malloc(threads*sizeof(struct thread_data));
     
     double *matrix = malloc(dimensionmatrix * dimensionmatrix * sizeof(double));
     double *reducedmatrix = malloc(((dimensionmatrix * dimensionmatrix))*sizeof(double));
-//    double *slidingreducedmatrix = malloc((dimensionmatrix-1)*(dimensionmatrix-1)*sizeof(double));
+    double *slidingreducedmatrix = malloc((dimensionmatrix-1)*(dimensionmatrix-1)*sizeof(double));
     // Initialize our first matrix.
     //
     printf("Initial matrix: \n");
     initializematrix(matrix,dimensionmatrix);
-    initializematrix(reducedmatrix,dimensionmatrix/2);
+    //initializematrix(reducedmatrix,dimensionmatrix/2);
     printmatrix(matrix,dimensionmatrix);
 
+
+    //Setup and create threads for non overlapping window.
     for(int i=0;i<threads;i++){
 
         thread_desc[i].bounds.imin=i*((dimensionmatrix/reduction)/threads);
@@ -145,31 +137,56 @@ int main(int argc, char *argv[]){
         thread_desc[i].bounds.jmax=dimensionmatrix/reduction-1;
         thread_desc[i].dimensions=dimensionmatrix;
         thread_desc[i].reduction=reduction;
-
         thread_desc[i].inputmatrix = &matrix;
-        thread_desc[i].outputmatrix = &reducedmatrix;
+        thread_desc[i].outputmatrix = &reducedmatrix;   
+
         
-        if(pthread_create(&thread_desc[i].thread_id, NULL, matrixreduce, &thread_desc[i])) errx(1,"Thread creation failed.");
+        if(pthread_create(&thread_desc[i].thread_id, NULL, matrixreduce_worker, &thread_desc[i])) errx(1,"Thread creation failed.");
     }
 
 
+    //Join threads (wait for them to return before moving on
     for(int i=0;i<threads;i++){
         pthread_join(thread_desc[i].thread_id,NULL);
+        
     }
 
+    //Setup and create threads for overlapping window.
+    for(int i=0;i<threads;i++){
 
-    //struct arrayindex reducearrayindex = {0};
-    //matrixreduce(matrix,reducedmatrix,dimensionmatrix,reductions,reducearrayindex);
 
+        thread_desc_slide[i].bounds.imin=i*(dimensionmatrix/threads);
+        thread_desc_slide[i].bounds.imax=(i+1)*(dimensionmatrix/threads)-1;
+        thread_desc_slide[i].bounds.jmin=0;
+        thread_desc_slide[i].bounds.jmax=dimensionmatrix-1;
+        thread_desc_slide[i].dimensions=dimensionmatrix;
+        thread_desc_slide[i].reduction = reduction;
+        thread_desc_slide[i].inputmatrix=&matrix;
+        thread_desc_slide[i].outputmatrix = &slidingreducedmatrix;
 
+        if(pthread_create(&thread_desc_slide[i].thread_id, NULL, slidingmatrixreduce_worker, &thread_desc_slide[i])) errx(1, "Thread creation failed.");
+
+    }
+
+    //Join threads
+    for(int i=0;i<threads;i++){
+        pthread_join(thread_desc_slide[i].thread_id,NULL);
+        
+    }
+
+    //Print out our matrices
     printf("\n No overlap reduced matrix: \n");
-    //printmatrix(reducedmatrix,2);
-/*
-    slidingmatrixreduce(matrix,slidingreducedmatrix,dimensionmatrix,reductions);
+    printmatrix(reducedmatrix,dimensionmatrix/reduction);
     printf("\n Sliding window reduced matrix: \n");
     printmatrix(slidingreducedmatrix,dimensionmatrix-1);
-*/
 
 
-return 0;
+    //Free up anything we allocated on the heap
+    free(matrix);
+    free(reducedmatrix);
+    free(slidingreducedmatrix);
+    free(thread_desc);
+    free(thread_desc_slide);
+
+    return 0;
 }
