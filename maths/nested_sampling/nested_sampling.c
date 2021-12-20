11958c2d6ee0b79fa98583176758d7cbbbbd2ad4
James Kent <jameskent@Jamess-MacBook-Air.local>
2021-12-20 11:56:20 +0000

    Finish basic nested sampler

--- maths/nested_sampling/nested_sampling.c
+++ maths/nested_sampling/nested_sampling.c
@@ -7,11 +7,13 @@
 #include <math.h>
 #include <float.h>
 
-#define VERBOSE_DEBUG_MESSAGES ( 1 )
+#ifndef VERBOSE_DEBUG_MESSAGES
+#define VERBOSE_DEBUG_MESSAGES ( 0 )
+#endif
 
 #define N_DIM ( 2 ) // Dimensionality of the likelihood space
-#define N ( 100 ) // Number of atoms
-#define MAX ( 1000 ) // Max number of iterations
+#define N ( 200 ) // Number of atoms
+#define MAX ( 2000 ) // Max number of iterations
 
 #define UNIFORM ( ( rand() + 0.5 ) / ( RAND_MAX + 1.0 ) ) // Defined on domain (0,1) 
 #define PLUS(x,y) ( x > y ? x + log( 1 + exp( y - x ) ) : y + log( 1 + exp( x - y ) ) )
@@ -26,63 +28,21 @@ typedef struct nestedObject {
 // Functions encapsulating knowledge of our model, prior and results
 #define MODEL_MEAN ( 20.0 ) // Mean the same in all dimensions
 #define MODEL_SIGMA ( 8.0 )  // Standard deviation the same in all dimensions
-double _model(nestedObject *object)
-{
-    double normalisation_constant = 1.0 / ( sqrt( 2 * M_PI ) * MODEL_SIGMA );
-    double exponent = 0.0;
-    for( int i = 0; i < N_DIM; ++i )
-    {
-        double coord = object->x[i];
-	exponent -= ( pow( ( coord - MODEL_MEAN ) / MODEL_SIGMA , 2 ) );
-    }
-    exponent *= (double)( N_DIM / 2 );
-    return log( exp( exponent ) );
-}
+#define MODE_SHIFT ( 17.6 ) // Used for a second mode in multimodal model.
 
-#define PRIOR_BOX ( 10.0 )
+double _model( nestedObject *object ); // Basic gaussian model
+double _multimodal_model( nestedObject *object ); // Bi-model gaussian model
+#define LIKELIHOOD( object ) ( _multimodal_model( object ) );
+
+#define PRIOR_BOX ( 30.0 )
 #define PRIOR_MIN ( MODEL_MEAN - PRIOR_BOX )
 #define PRIOR_MAX ( MODEL_MEAN + PRIOR_BOX )
-void Prior(nestedObject *object)
-{
-    for(int i = 0; i < N_DIM; ++i)
-    {
-	double randv = UNIFORM;
-	fprintf( stderr, "UNIFORM:%g\n", randv );
-	object->x[i] = ( randv - 0.5 ) * ( 2 * PRIOR_BOX ) + MODEL_MEAN;// - ( 2 * PRIOR_BOX - MODEL_MEAN );
-    }
-    object->log_likelihood = _model( object );
-}
+void Prior(nestedObject *object); // Sets up an object within our prior boundaries
 
 // For re-nesting an object after it has been killed.
-// We use a very crude genetic algorithm to do this by mixing co-ords from two random objects.
 void Explore(nestedObject *object,  // Object we want to replace 
 	     nestedObject *objects, // Objects 
-	     double logLstar)
-{
-    double evolvedLogL = 0.0;
-    nestedObject dummy;
-    do
-    {
-	int parents[2] = { 0 };
-	parents[0] = (int)( N * UNIFORM ) % N;
-	parents[1] = (int)( N * UNIFORM ) % N;
-
-	if( parents[0] == parents[1] ) continue; // We can not reproduce asexually.
-
-    	for( int i = 0; i < N_DIM; ++i )
-	{
-	    int gene_to_use = (int)( 2 * UNIFORM ) % 2;
-	    dummy.x[i] = objects[parents[gene_to_use]].x[i];
-	}
-        evolvedLogL = _model( &dummy );
-	
-    }
-    while( evolvedLogL < logLstar );
-    dummy.log_likelihood = evolvedLogL;
-    //    fprintf( stderr, "New logL:%g\n", evolvedLogL );
-    dummy.logwidth = 0.0;
-    *object = dummy;
-}
+	     double logLstar); // The new log likelihood limit.
 
 int main(void)
 {
@@ -118,7 +78,7 @@ int main(void)
     // START - Nested Sampling Loop
     for( nest = 0; nest < MAX; ++nest )
     {
-	fprintf( stderr, "nest:%d logwidth:%g\n", nest, logwidth );
+	//fprintf( stderr, "nest:%d logwidth:%g\n", nest, logwidth );
 	worst = 0;
 	for( i = 1; i < N; ++i )
         {
@@ -161,5 +121,86 @@ int main(void)
     printf( "# iterations = %d\n", nest );
     printf( "Evidence: ln(Z) = %g +- %g\n", logZ, sqrt( H / N ) );
     printf( "Information: H = %g nats = %g bits\n", H, H / log( 2. ) );
+    printf( "Final posterior sample: \n" );
+    for(int i = 0; i < N_DIM; ++i) printf( "Coord %d:%g\n", i, samples[nest].x[i] );
+    printf( "logL:%g +- %g\n", samples[nest].log_likelihood, sqrt( H / N ) );
     return 0;
+}
+
+// Basic gaussian model
+double _model(nestedObject *object)
+{
+    double exponent = 0.0;
+    for( int i = 0; i < N_DIM; ++i )
+    {
+        double coord = object->x[i];
+	exponent -= ( pow( ( coord - MODEL_MEAN ) / MODEL_SIGMA , 2 ) );
+    }
+    exponent *= (double)( N_DIM / 2 );
+    return 1 + log( exp( exponent ) );
+}
+
+// Slightly more elaborate multi-model gaussian model
+double _multimodal_model(nestedObject *object)
+{
+    double exponent1 = 0.0;
+    double exponent2 = 0.0;
+    // Mode 1
+    for( int i = 0; i < N_DIM; ++i )
+    {
+        double coord = object->x[i];
+	exponent1 -= ( pow( ( coord - MODEL_MEAN ) / MODEL_SIGMA, 2 ) );
+    }
+    exponent1 *= (double)( N_DIM / 2 );
+
+    // Mode 2
+    for( int i = 0; i < N_DIM; ++i )
+    {
+	double coord = object->x[i];
+	exponent2 -= ( pow( ( coord - MODEL_MEAN - MODE_SHIFT  ) / MODEL_SIGMA, 2 ) );
+    }
+    exponent2 *= (double)( N_DIM / 2 );
+
+    return 1 + log( exp ( exponent1 ) + 1.1 * exp( exponent2 ) );
+}
+
+void Prior(nestedObject *object)
+{
+    for(int i = 0; i < N_DIM; ++i)
+    {
+	object->x[i] = ( UNIFORM - 0.5 ) * ( 2 * PRIOR_BOX ) + MODEL_MEAN;// - ( 2 * PRIOR_BOX - MODEL_MEAN );
+    }
+    object->log_likelihood = LIKELIHOOD( object );
+}
+
+// For re-nesting an object after it has been killed.
+// We use a very crude genetic algorithm to do this by mixing co-ords from two random objects.
+void Explore(nestedObject *object,  // Object we want to replace 
+	     nestedObject *objects, // Objects 
+	     double logLstar)
+{
+    double evolvedLogL = 0.0;
+    nestedObject dummy;
+    do
+    {
+	int parents[2] = { 0 };
+	parents[0] = (int)( N * UNIFORM ) % N;
+	parents[1] = (int)( N * UNIFORM ) % N;
+
+	if( parents[0] == parents[1] ) continue; // We can not reproduce asexually.
+
+    	for( int i = 0; i < N_DIM; ++i )
+	{
+            double mixing_factor = UNIFORM;
+	    double new_gene = mixing_factor * objects[parents[0]].x[i] +
+		             ( 1 - mixing_factor ) * objects[parents[1]].x[i];
+	    dummy.x[i] = new_gene;
+	}
+        evolvedLogL = LIKELIHOOD( &dummy );	
+    }
+    while( evolvedLogL < logLstar );
+    dummy.log_likelihood = evolvedLogL;
+    //    fprintf( stderr, "New logL:%g\n", evolvedLogL );
+    dummy.logwidth = 0.0;
+    *object = dummy;
 }
